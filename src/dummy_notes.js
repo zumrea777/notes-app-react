942972af36eb372dceebdd87653bbd37a629d45c
zumrea777 <acarzmrete@yandex.com>
2024-07-10 17:01:27 +0300

    first commit

---
+++ src/dummy_notes.js
@@ -0,0 +1,92 @@
+import { LuFolderMinus } from "react-icons/lu";
+
+const notes = [
+    {
+        id: 1,
+        title: "how to make personal brand stand out online",
+        details:
+            "Lorem ipsum dolor sit amet consectetur",
+        date: "Jan 1, 2024",
+    },
+
+{
+        id: 2,
+        title: "how to make personal brand stand out online",
+        details:
+            "Lorem ipsum dolor sit amet consectetur",
+        date: "Jan 2, 2024",
+    },
+
+    {
+        id: 3,
+        title: "how to make personal brand stand out online",
+        details:
+            "Lorem ipsum dolor sit amet consectetur",
+        date: "Jan 3, 2024",
+    },
+
+        {
+        id: 4,
+        title: "how to make personal brand stand out online",
+        details:
+            "Lorem ipsum dolor sit amet consectetur",
+        date: "Jan 4 2024",
+    },
+
+
+            {
+        id: 5,
+        title: "how to make personal brand stand out online",
+        details:
+            "Lorem ipsum dolor sit amet consectetur",
+        date: "Jan 5, 2024",
+    },
+
+
+
+                {
+        id: 6,
+        title: "how to make personal brand stand out online",
+        details:
+            "Lorem ipsum dolor sit amet consectetur",
+        date: "Jan 6, 2024",
+    },
+
+
+                    {
+        id: 7,
+        title: "how to make personal brand stand out online",
+        details:
+            "Lorem ipsum dolor sit amet consectetur",
+        date: "Jan 7, 2024",
+    },
+
+                        {
+        id: 8,
+        title: "how to make personal brand stand out online",
+        details:
+            "Lorem ipsum dolor sit amet consectetur",
+        date: "Jan 8, 2024",
+    },
+
+                            {
+        id: 9,
+        title: "how to make personal brand stand out online",
+        details:
+            "Lorem ipsum dolor sit amet consectetur",
+        date: "Jan 9, 2024",
+    },
+
+                                {
+        id: 10,
+        title: "how to make personal brand stand out online",
+        details:
+            "Lorem ipsum dolor sit amet consectetur",
+        date: "Jan 10, 2024",
+    },
+
+
+
+];
+
+export default notes;
